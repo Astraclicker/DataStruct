b064f6111bdacca910a36057453ea2d0143293f9
Astraclicker <2867314230@qq.com>
2026-05-23 21:45:52 +0800

    修改gitignore文件

---
+++ build/CMakeFiles/DataStruct.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStruct.dir/src/main.cpp.obj"
+  "CMakeFiles/DataStruct.dir/src/main.cpp.obj.d"
+  "DataStruct.exe"
+  "DataStruct.exe.manifest"
+  "DataStruct.pdb"
+  "libDataStruct.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
